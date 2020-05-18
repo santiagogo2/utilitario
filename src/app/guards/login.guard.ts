3975dc0c1ea7350688482871947225bbb1da1777
Santiago Ramirez Gaitan <santiagooo42@gmail.com>
2020-05-17 23:55:11 -0500

    Sin cambios

---
+++ src/app/guards/login.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { UserService } from '../services/service.index';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+	constructor(
+		private _userService: UserService,
+		private _router: Router
+	){}
+
+	canActivate(){
+		let identity = this._userService.getIdentity();
+
+		if(identity){
+			this._router.navigate(['/dashboard']);
+			return false;
+		} else{
+			return true;
+		}
+	} 
+  
+}
