0ceb2c1739435ff6c73158be7dcf8d1b74df62d2
Santiago Ramirez Gaitan <santiagooo42@gmail.com>
2020-05-23 10:03:32 -0500

    Modulo de unidades

---
+++ src/app/admin/pages/sala-situacional/unit/unit.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+// Components
+import { UnitListComponent } from './unit-list/unit-list.component';
+import { UnitRegisterComponent } from './unit-register/unit-register.component';
+import { UnitEditComponent } from './unit-edit/unit-edit.component';
+
+@NgModule({
+	declarations:[
+		UnitListComponent,
+		UnitRegisterComponent,
+		UnitEditComponent,
+	],
+	imports:[
+		FormsModule,
+		HttpClientModule,
+		NgxPaginationModule,
+	],
+	exports:[
+		UnitListComponent,
+		UnitRegisterComponent,
+		UnitEditComponent,
+	],
+})
+export class UnitModule { }
